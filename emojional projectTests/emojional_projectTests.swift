3062b8d055a9e2d35753fcde3aef83ef91dbac6d
Lizzie High <lizziehigh02@gmail.com>
2019-07-23 13:27:52 -0400

    Initial Commit

---
+++ emojional projectTests/emojional_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  emojional_projectTests.swift
+//  emojional projectTests
+//
+//  Created by Lizzie High on 7/23/19.
+//  Copyright © 2019 Lizzie High. All rights reserved.
+//
+
+import XCTest
+@testable import emojional_project
+
+class emojional_projectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
